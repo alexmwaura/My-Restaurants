6c081472f292b1c25a0252270cc35ca04ca8b764
alexmwaura <alexmwaura43@gmail.com>
2020-11-25 13:44:25 +0300

    add intent to RestaurantsActivity

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -6,19 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
+        mLocationEditText = (EditText)findViewById(R.id.locationEditText);
+
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String location = mLocationEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this,RestaurantsActivity.class);
+                intent.putExtra("location",location);
                 startActivity(intent);
             }
         });
