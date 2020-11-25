fa7daa094a308068b377043ba9e0500ece0891d0
alexmwaura <alexmwaura43@gmail.com>
2020-11-25 13:44:01 +0300

    add test case

--- app/src/androidTest/java/com/moringaschool/myrestaurants/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/moringaschool/myrestaurants/MainActivityInstrumentationTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -21,7 +23,20 @@ public class MainActivityInstrumentationTest {
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
     @Test
     public void validateEditText(){
-        onView(withId(R.id.locationEditText)).perform(typeText("Portland"))
-                .check(matches(withText("Portland")));
+        onView(withId(R.id.locationEditText)).perform(typeText("Nairobi"))
+                .check(matches(withText("Nairobi")));
+    }
+    @Test
+    public void locationIsSentToRestaurantsActivity(){
+        String location = "Nairobi";
+        onView(withId(R.id.locationEditText)).perform(typeText(location)).perform(closeSoftKeyboard());
+        try {
+            Thread.sleep(250);
+        }
+        catch (InterruptedException e){
+            System.out.println("got interrupted!");
+        }
+        onView(withId(R.id.findRestaurantsButton)).perform(click());
+        onView(withId(R.id.locationTextView)).check(matches((withText("Here are all the restaurants near: " + location))));
     }
 }
