8f5c0c6f9e20dba8ad36c5990026a90f5bb2c2bb
alexmwaura <alexmwaura43@gmail.com>
2020-11-26 22:20:44 +0300

    add butterKnife

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -7,18 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText)  EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
-        mLocationEditText = (EditText)findViewById(R.id.locationEditText);
-
+        ButterKnife.bind(this);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
